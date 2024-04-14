674e491041024936d7974daf2b81c02c1d2331f6
Sergey Grachev <grachevse2405@gmail.com>
2024-04-14 23:22:34 +0300

    Add Dilema::playGame()

--- Dilema.cpp
+++ Dilema.cpp
@@ -2,5 +2,34 @@
 
 Dilema::Dilema()
 {
+    m_balance = 0;
+}
 
+void Dilema::playGame(Dilema *oponent, bool silentOponent, bool silent)
+{
+    if(silentOponent && silent) {
+        m_history.push_back(3);
+        m_balance += 3;
+        oponent->m_history.push_back(3);
+        oponent->m_balance += 3;
+
+    } else if(silentOponent && !silent) {
+        m_history.push_back(0);
+        oponent->m_history.push_back(5);
+        oponent->m_balance += 5;
+    } else if(!silentOponent && silent) {
+        m_history.push_back(5);
+        m_balance += 3;
+        oponent->m_history.push_back(0);
+    } else {
+        m_history.push_back(1);
+        m_balance += 1;
+        oponent->m_history.push_back(1);
+        oponent->m_balance += 1;
+    }
+}
+
+QList<qint8> Dilema::history()
+{
+    return m_history;
 }
--- Dilema.h
+++ Dilema.h
@@ -1,11 +1,21 @@
 #ifndef DILEMA_H
 #define DILEMA_H
 
+#include <QObject>
 
 class Dilema
 {
 public:
     Dilema();
+//    virtual ~Dilema;
+
+    void playGame(Dilema *oponent,  bool silentOponent, bool silent);
+    QList<qint8> history();
+
+protected:
+    QList<qint8> m_history;
+    int m_balance;
+
 };
 
 #endif // DILEMA_H
